030529a37ef8b608a5c79f503b779851c67592f2
2547techno <technowizz2547@gmail.com>
2025-09-22 00:12:08 -0400

    ci(nix): update commit

--- package.nix
+++ package.nix
@@ -3,12 +3,12 @@
 }:
 
 let
-  gitHash = "1f94fbe99190835e9f8f035797ae0b30229db14d";
+  gitHash = "34230a15dd56c688c3bfe7ca5d31a86a7358f6ca";
   repo = pkgs.fetchFromGitHub {
     owner = "2547techno";
     repo = "technorino";
     rev = gitHash;
-    hash = "sha256-EjyeCVu9gqZXULKaAqaDigdI4WQgj2te9Rp/4NQ/lnA=";
+    hash = "sha256-XR+uEkb+UiBLs0wFRFsafVB+896ZrDioiLAAng8RoJ0=";
     fetchSubmodules = true;
   };
   technorino = import "${repo}/default.nix" { inherit pkgs gitHash; };
