106b2d754d5bc82f78ce012d08d085cd27930f71
2547techno <technowizz2547@gmail.com>
2025-09-22 23:53:23 -0400

    nix: update package

--- package.nix
+++ package.nix
@@ -3,12 +3,12 @@
 }:
 
 let
-  gitHash = "7c3e3c4a03cd909c05ab06d5dd0d115d600caa47";
+  gitHash = "0f6e606b996aad13784dab98a9a9ddd3fab0a205";
   repo = pkgs.fetchFromGitHub {
     owner = "2547techno";
     repo = "technorino";
     rev = gitHash;
-    hash = "sha256-0/Wsx4PA2K+2lzuZ6wL2XGZe1HRx1o5Hn2KTcBfyw+o=";
+    hash = "sha256-ifDR336OWS8udFIxysOMuhzjymyRASdAcTAie5Qef1A=";
     fetchSubmodules = true;
   };
   technorino = import "${repo}/default.nix" { inherit pkgs gitHash; };
